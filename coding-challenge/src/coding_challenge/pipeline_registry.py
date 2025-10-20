8e2f5eeb4e4a47a54613340b2fa9a686e65e649a
MerlinSchaefer <ms101196@googlemail.com>
2025-10-20 08:33:16 +0200

    Add silver with teigling to brötchen conversion

--- coding-challenge/src/coding_challenge/pipeline_registry.py
+++ coding-challenge/src/coding_challenge/pipeline_registry.py
@@ -3,7 +3,7 @@
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 from coding_challenge.pipelines.bronze.pipeline import create_pipeline as bronze_create 
-
+from coding_challenge.pipelines.silver.pipeline import create_pipeline as silver_create
 
 # def register_pipelines() -> dict[str, Pipeline]:
 #     """Register the project's pipelines.
@@ -17,7 +17,10 @@ from coding_challenge.pipelines.bronze.pipeline import create_pipeline as bronze
 #     return pipelines
 def register_pipelines() -> dict[str, Pipeline]:
     bronze = bronze_create()
+    silver = silver_create()
+    default = bronze + silver
     return {
-        "__default__": bronze,        # optional
+        "__default__": default,
         "01_bronze": bronze,
+        "02_silver": silver,
     }
--- coding-challenge/src/coding_challenge/pipelines/silver/pipeline.py
+++ coding-challenge/src/coding_challenge/pipelines/silver/pipeline.py
@@ -1,18 +1,53 @@
-from kedro.pipeline import Pipeline, node
-from .nodes import check_sales_not_empty
-    
-def create_pipeline(**kwargs):
-    return Pipeline(
-        [
-            node(
-                func=check_sales_not_empty,
-                inputs=[
-                    "01_sales",
-                    "parameters",
-                ],
-                outputs="99_EmptySalesLog",
-                name="check_for_empty_sales",
-                confirms="01_sales",
-            ),
-        ]
-    )
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import (
+    union_mapping_product,
+    union_mapping_store,
+    build_silver_products,
+    build_silver_stores,
+    build_silver_sales_daily,
+)
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        # 1) Mappings union (je Entität)
+        node(
+            union_mapping_product,
+            inputs=["raw_1001_mapping_product", "raw_1002_mapping_product", "raw_1003_mapping_product"],
+            outputs="mapping_product_union",
+            name="silver_union_mapping_product",
+        ),
+        node(
+            union_mapping_store,
+            inputs=["raw_1001_mapping_store", "raw_1002_mapping_store", "raw_1003_mapping_store"],
+            outputs="mapping_store_union",
+            name="silver_union_mapping_store",
+        ),
+
+        # 2) Dimensions
+        node(
+            build_silver_products,
+            inputs=["bronze.products_all", "mapping_product_union"],
+            outputs="silver.products",
+            name="silver_build_products",
+        ),
+        node(
+            build_silver_stores,
+            inputs=["bronze.stores_all", "mapping_store_union"],
+            outputs="silver.stores",
+            name="silver_build_stores",
+        ),
+
+        # 3) Daily fact with stockout
+        node(
+            build_silver_sales_daily,
+            inputs=[
+                "bronze.sales_all",
+                "bronze.deliveries_all",
+                "raw_1001_mapping_delivery2sales", # neededed for stockout logic
+                "mapping_product_union",   # <-- pass mappings, not dims
+                "mapping_store_union",
+            ],
+            outputs="silver.sales_daily",
+            name="silver_build_sales_daily",
+        ),
+    ])
